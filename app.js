bda39ab41abfd85866751d9cc5660d445a11a4b2
Peter Chen <peter@weme.sh>
2016-06-01 22:36:26 -0400

    Print out id type

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ io.on('connection', function(socket) {
 		pinger = setInterval(function() {
 			console.log('Send Ping request');
 			var id = Date.now();
+			console.log('ID Type: ' + id.prototype);
 			records[device][id] = -1;
 			socket.emit('ping', id);
 		}, 5000);
@@ -22,9 +23,10 @@ io.on('connection', function(socket) {
 		clearInterval(pinger);
 	});
 
-	socket.on('pong', function(device, id){
+	socket.on('pong', function(device, id) {
+		console.log('ID Type: ' + id.prototype);
 		console.log('Got Ping response');
-		records[device][id] = Date.now() - id;
+		records[device][parseInt(id)] = Date.now() - id;
 	});
 
 	socket.on('disconnect', function(data){
