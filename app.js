92ed3cd44ef41d0046cfa6b9b97eadb6938a401f
Peter Chen <peter@weme.sh>
2016-06-01 22:40:58 -0400

    toString

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ io.on('connection', function(socket) {
 		pinger = setInterval(function() {
 			console.log('Send Ping request');
 			var id = Date.now();
-			console.log('ID Type: ' + id.prototype);
+			console.log('Sent ID Type: ' + id.prototype.toString.call(t));
 			records[device][id] = -1;
 			socket.emit('ping', id);
 		}, 5000);
@@ -24,7 +24,7 @@ io.on('connection', function(socket) {
 	});
 
 	socket.on('pong', function(device, id) {
-		console.log('ID Type: ' + id.prototype);
+		console.log('Received ID Type: ' + id.prototype.toString.call(t));
 		console.log('Got Ping response');
 		records[device][parseInt(id)] = Date.now() - id;
 	});
